80419b9a3eb6fefdb14e72adc134f83e039617c9
Nabeel on the Lenovo <you@example.com>
2019-05-19 13:00:40 +0100

    added footer

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,7 +1,7 @@
 import React from "react"
 import './all.scss';
 import Navbar from "./Navbar";
-import Footer from "./footer";
+import Footer from "./Footer";
 
 export default ({ children }) => {
   return <div>    
