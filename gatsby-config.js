0bf041715b0022d0ac236d74b8525c9eaa96a941
Nabeel on the Lenovo <you@example.com>
2019-05-26 12:53:40 +0100

    added seo

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,11 +6,12 @@
 
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'Nova Hajj Umrah',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'With many years of experience, Nova Hajj Umrah provides Hajj and Umrah Services. Based in Cardiff, we make sure to arrange visa, tickets and food so you can concentrate on your prayers.'
   },
   plugins: [
+    'gatsby-plugin-react-helmet',
     'gatsby-plugin-sass',
     {
       // keep as first gatsby-source-filesystem plugin for gatsby image support
